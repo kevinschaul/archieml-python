b74d0119d5b267a64ce225aa0ad3095a80826959
Kevin Schaul <kevin.schaul@gmail.com>
2015-04-22 23:54:43 -0400

    Add parse test

--- tests/test_archieml.py
+++ tests/test_archieml.py
@@ -13,3 +13,15 @@ class TestLexer(unittest.TestCase):
         expected = "[LexToken(IDENTIFIER,'key',1,0),LexToken(COLON,':',1,3),LexToken(TEXT,'This is a value',1,5)]"
         output = str(self.parser.tokenize(archieml))
         self.assertEqual(expected, output)
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser()
+
+    def test_keyValue(self):
+        archieml = 'key: This is a value'
+        expected = {
+            'key': 'This is a value'
+        }
+        output = self.parser.parse(archieml)
+        self.assertEqual(expected, output)
